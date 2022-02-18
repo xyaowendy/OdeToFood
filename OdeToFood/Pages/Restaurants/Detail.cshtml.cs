2ddd7ec893fe422e272a5396e8179f3c31477d79
Wendy Jiang <Wendy.Jiang@cpal.com.au>
2022-02-18 23:43:09 +1000

    Added a restaurants detail page

---
+++ OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFoodCore;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class Detail : PageModel
+    {
+        public Restaurant Restaurant { get; set; }
+        
+        public void OnGet()
+        {
+            Restaurant = new Restaurant();
+        }
+    }
+}
