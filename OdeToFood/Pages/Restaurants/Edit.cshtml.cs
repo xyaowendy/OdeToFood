93eb8baacdd469e7742f78e62dbf6d496eef4cfe
Wendy Jiang <Wendy.Jiang@cpal.com.au>
2022-02-19 16:30:57 +1000

    Created a edit page for restaurant details

---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Data;
+using OdeToFoodCore;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class Edit : PageModel
+    {
+        private readonly IRestaurantData _restaurantData;
+        private readonly IHtmlHelper _htmlHelper;
+
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public Edit(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
+        {
+            _restaurantData = restaurantData;
+            _htmlHelper = htmlHelper;
+        }
+        
+        public IActionResult OnGet(int restaurantId)
+        {
+            Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
+            
+            Restaurant = _restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+    }
+}
